52e05506f5ff621460a264c6f331098903902b0d
Edwin N. Johnson <edwinj.2010@yahoo.com>
2015-07-15 21:19:38 -0600

    Scripts to synchronize between emacs HOME and back to VCS

---
+++ sync_from_home.cmd
@@ -0,0 +1,3 @@
+robocopy %HOME%\.emacs.d .\.emacs.d /mir
+copy /q %HOME%\.emacs .\.emacs
+copy /q %HOME%\.custom .\.custom
---
+++ sync_to_home.cmd
@@ -0,0 +1,3 @@
+robocopy .\.emacs.d %HOME%\.emacs.d /mir
+copy /q .emacs %HOME%\.emacs
+copy /q .custom %HOME%\.custom
